e4da9ebf57ea413e860f590a0454999dead65795
Daniele Di Pinto <danieledipinto.c@gmail.com>
2023-11-23 17:42:38 +0100

    ux update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { subjects } from "@/lib/subjects";
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-lg">
           <h1 className="text-7xl font-bold">STEAM who?</h1>
@@ -13,25 +13,8 @@ export default function Home() {
             sai pensando!
           </p>
 
-          <div className="dropdown dropdown-hover py-2">
-            <label tabIndex={0} className="btn btn-ghost ">
-              ...
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content z-[1] menu p-2 pt-4 shadow bg-base-100 rounded-box w-52 space-y-2"
-            >
-              <li className="font-bold mb-2">Personaggi disponibili</li>
-              <hr />
-              {subjects.map((subject) => (
-                <li key={subject} className="hover:bg-base-200 px-1 py-1">
-                  {subject}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <Link href="/quiz" className="btn btn-primary">
+          <div className="py-6">
+            <Link href="/personaggi" className="btn btn-primary">
               VAI AL QUIZ
             </Link>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { themes } from "./src/lib/themes";
 
 const config: Config = {
   content: [
@@ -8,37 +9,7 @@ const config: Config = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-    ],
+    themes,
   },
 };
 export default config;
